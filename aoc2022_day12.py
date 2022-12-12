6f046adaaf8d7171534fd37d73027cdf68f57ef5
aconconi <aconconi@users.noreply.github.com>
2022-12-12 20:30:35 +0100

    refactoring, got rid of individual coordinates

--- aoc2022_day12.py
+++ aoc2022_day12.py
@@ -5,44 +5,27 @@
 
 from collections import deque
 
-# pylint: skip-file
 import pytest
 
 DELTA = {(0, 1), (0, -1), (-1, 0), (1, 0)}
 
-TRANSLATE_ELEV = str.maketrans("SE", "az")
+TRANS_ELEV = str.maketrans("SE", "az")
 
 
-"""
-The elevation of the destination square can be at most one higher
-than the elevation of your current square; that is, if your
-current elevation is m, you could step to elevation n, but not
-to elevation o. (This also means that the elevation of the destination
-square can be much lower than the elevation of your current square.)
-"""
-
-
-def neighbors(r, c, n_rows, n_cols):
-    yield from (
-        (r + dr, c + dc)
-        for dr, dc in DELTA
-        if 0 <= r + dr < n_rows and 0 <= c + dc < n_cols
-    )
+def neighbors(grid, current):
+    r, c = current
+    yield from ((nr, nc) for dr, dc in DELTA if (nr := r + dr, nc := c + dc) in grid)
 
 
 def can_step_uphill(current_h, neigh_h):
     return (
-        ord(neigh_h.translate(TRANSLATE_ELEV))
-        - ord(current_h.translate(TRANSLATE_ELEV))
-        <= 1
+        ord(neigh_h.translate(TRANS_ELEV)) - ord(current_h.translate(TRANS_ELEV)) <= 1
     )
 
 
 def can_step_downhill(current_h, neigh_h):
     return (
-        ord(neigh_h.translate(TRANSLATE_ELEV))
-        - ord(current_h.translate(TRANSLATE_ELEV))
-        >= -1
+        ord(neigh_h.translate(TRANS_ELEV)) - ord(current_h.translate(TRANS_ELEV)) >= -1
     )
 
 
@@ -51,31 +34,28 @@ def parse_input(file_name):
         data = data_file.read().splitlines()
         start = None
         goal = None
+        elev = {}
         for i, row in enumerate(data):
-            for j, current in enumerate(row):
-                if current == "S":
+            for j, square in enumerate(row):
+                if square == "S":
                     start = (i, j)
-                elif current == "E":
+                elif square == "E":
                     goal = (i, j)
-    return data, start, goal
+                elev[(i, j)] = square
+    return elev, start, goal
 
 
-def shortest_path(data, start, target_elevations, can_step):
-    num_rows, num_cols = len(data), len(data[0])
+def shortest_path_bfs(elev, start, target_elevations, can_step):
     queue = deque([start])
     dist = {start: 0}
     while queue:
         current = queue.popleft()
-        r, c = current
-        if data[r][c] in target_elevations:
+        if elev[current] in target_elevations:
             return dist[current]
-        current_elevation = data[r][c]
-        for neigh in neighbors(r, c, num_rows, num_cols):
-            neigh_r, neigh_c = neigh
-            neigh_elevation = data[neigh_r][neigh_c]
+        for neigh in neighbors(elev, current):
             if neigh in dist:
                 continue
-            if not can_step(current_elevation, neigh_elevation):
+            if not can_step(elev[current], elev[neigh]):
                 continue
             dist[neigh] = dist[current] + 1
             queue.append(neigh)
@@ -83,11 +63,11 @@ def shortest_path(data, start, target_elevations, can_step):
 
 
 def day12_part1(data, start, _):
-    return shortest_path(data, start, "E", can_step_uphill)
+    return shortest_path_bfs(data, start, "E", can_step_uphill)
 
 
 def day12_part2(data, _, goal):
-    return shortest_path(data, goal, "Sa", can_step_downhill)
+    return shortest_path_bfs(data, goal, "Sa", can_step_downhill)
 
 
 @pytest.fixture(autouse=True, name="test_data")
